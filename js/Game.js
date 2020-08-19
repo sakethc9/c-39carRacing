5bb938282112b5ad47d214221801b2f0ed98bfdd
sakethc9 <sakethc9@gmail.com>
2020-08-19 20:43:45 +0530

    car racing c39

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Img);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Img)
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,16 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("grey")
+
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -63,6 +70,9 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x,y,60,60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
@@ -79,6 +89,12 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3750){
+      gameState = 2 ;
+    }
     drawSprites();
+  }
+  end (){
+    console.log("game ended");
   }
 }
